3fa8da44c7147070918f1f62c2e20e2a5dca1662
william <william@94bf062e-bdd7-4c11-962c-b781cd401d63>
2010-06-12 18:58:37 +0000

    Modificado el script para compilar xharbour en modo consola Modificado el prg eliminando el #if...#endif inicial
    git-svn-id: http://www.sitasoft.net/tdolphin@7 94bf062e-bdd7-4c11-962c-b781cd401d63

--- samples/bldtestx.bat
+++ samples/bldtestx.bat
@@ -1,97 +1,65 @@
-@ECHO OFF
-CLS
-if A%1 == A GOTO :SINTAX
-if NOT EXIST %1.prg GOTO :NOEXIST
-
-if "%HBDIR%" == "" set HBDIR=c:\xharbour
-rem if "%2" == "/b" set GT=gtwin
-rem if not "%2" == "/b" set GT=gtgui
-set GT=gtgui
-rem set GT=gtwin
-
+@echo off
 ECHO Compiling...
 
-set hdir=c:\xharbour
-set hdirl=%hdir%\lib
-set fwh=c:\fivewin\svn\repo
-set bcdir=c:\bcc582
+set wfile=testcon
+set hdir=y:\xharbcvs
+set bcdir=y:\borland\bcc55\bin
 
-%hdir%\bin\harbour %1 /n /i%fwh%\include;%hdir%\include /w /p %2 %3 > comp.log
-IF ERRORLEVEL 1 GOTO COMPILEERRORS
-@type comp.log
+%hdir%\bin\harbour %wfile% /n /i..\include;%hdir%\include /p %2 %3 > clip.log
+@type clip.log
+IF ERRORLEVEL 1 PAUSE
+IF ERRORLEVEL 1 GOTO EXIT
 
-echo -O2 -e%1.exe -I%hdir%\include -I%bcdir%\include %1.c > b32.bc
-%bcdir%\bin\bcc32 -M -c @b32.bc
+echo -O2 -e%wfile%.exe -I%hdir%\include %wfile%.c > b32.bc
+%bcdir%\bcc32 -M -c @b32.bc
 :ENDCOMPILE
 
-IF EXIST %1.rc %bcdir%\bin\brc32 -r %1
-rem IF EXIST %1.rc %vcdir%\bin\rc -r -d__FLAT__ %1
-
-echo %bcdir%\lib\c0w32.obj + > b32.bc
-echo %1.obj, + >> b32.bc
-echo %1.exe, + >> b32.bc
-echo %1.map, + >> b32.bc
-echo ..\lib\dolphinx.lib + >> b32.bc  
-echo ..\lib\libmysql.lib + >> b32.bc 
-echo %fwh%\lib\FiveHX.lib %fwh%\lib\FiveHC.lib + >> b32.bc
-echo %hdirl%\rtl.lib + >> b32.bc      
-echo %hdirl%\vm.lib + >> b32.bc       
-echo %hdirl%\%GT%.lib + >> b32.bc     
-echo %hdirl%\lang.lib + >> b32.bc     
-echo %hdirl%\macro.lib + >> b32.bc    
-echo %hdirl%\rdd.lib + >> b32.bc      
-echo %hdirl%\dbfntx.lib + >> b32.bc   
-echo %hdirl%\dbfcdx.lib + >> b32.bc   
-echo %hdirl%\dbffpt.lib + >> b32.bc   
-echo %hdirl%\hbsix.lib + >> b32.bc    
-echo %hdirl%\debug.lib + >> b32.bc    
-echo %hdirl%\common.lib + >> b32.bc   
-echo %hdirl%\pp.lib + >> b32.bc       
-echo %hdirl%\pcrepos.lib + >> b32.bc  
-rem echo %hdirl%\ct.lib + >> b32.bc        
-
+echo c0x32.obj + > b32.bc
+echo %wfile%.obj, + >> b32.bc
+echo %wfile%.exe, + >> b32.bc
+echo %wfile%.map, + >> b32.bc
+echo dolphinx.lib + >> b32.bc
+echo ..\lib\libmysql.lib + >> b32.bc
+echo %hdir%\lib\rtl.lib + >> b32.bc
+echo %hdir%\lib\hsx.lib + >> b32.bc
+echo %hdir%\lib\hbsix.lib + >> b32.bc
+echo %hdir%\lib\rtl.lib + >> b32.bc
+echo %hdir%\lib\vm.lib + >> b32.bc
+echo %hdir%\lib\rtl.lib + >> b32.bc
+echo %hdir%\lib\gtwin.lib + >> b32.bc
+echo %hdir%\lib\Lang.lib + >> b32.bc
+echo %hdir%\lib\macro.lib + >> b32.bc
+echo %hdir%\lib\rdd.lib + >> b32.bc
+echo %hdir%\lib\dbfntx.lib + >> b32.bc
+echo %hdir%\lib\dbfcdx.lib + >> b32.bc
+echo %hdir%\lib\common.lib + >> b32.bc
+echo %hdir%\lib\libmisc.lib + >> b32.bc
+echo %hdir%\lib\CodePage.lib + >> b32.bc
+echo %hdir%\lib\DbfFpt.lib + >> b32.bc
+echo %hdir%\lib\pcrepos.lib + >> b32.bc
 
 rem Uncomment these two lines to use Advantage RDD
-rem echo %hdirl%\rddads.lib + >> b32.bc
-rem echo %hdirl%\Ace32.lib + >> b32.bc
+rem echo %hdir%\lib\rddads.lib + >> b32.bc 
+rem echo %hdir%\lib\ace32.lib + >> b32.bc
 
 echo %bcdir%\lib\cw32.lib + >> b32.bc
-echo %bcdir%\lib\uuid.lib + >> b32.bc
-echo %bcdir%\lib\import32.lib + >> b32.bc
-echo %bcdir%\lib\psdk\odbc32.lib + >> b32.bc
-echo %bcdir%\lib\psdk\nddeapi.lib + >> b32.bc
-echo %bcdir%\lib\psdk\iphlpapi.lib + >> b32.bc
-echo %bcdir%\lib\psdk\msimg32.lib + >> b32.bc
-echo %bcdir%\lib\psdk\rasapi32.lib, >> b32.bc
+echo %bcdir%\lib\import32.lib, >> b32.bc
 
-IF EXIST %1.res echo %1.res >> b32.bc
-if %GT% == gtwin %bcdir%\bin\ilink32 -Tpe -s @b32.bc
+ECHO * 
+ECHO Linking...
+%bcdir%\ilink32 -Gn -Tpe -s @b32.bc
+
+rem delete temporary files
+rem @del %wfile%.c
+
 IF ERRORLEVEL 1 GOTO LINKERROR
-if %GT% == gtgui %bcdir%\bin\ilink32 -Gn -aa -Tpe -s @b32.bc
-IF ERRORLEVEL 1 GOTO LINKERROR
-ECHO * Application successfully built *
-%1
+ECHO * Application successfully built
 GOTO EXIT
 ECHO
 
-:COMPILEERRORS
-@type comp.log
-ECHO * Compile errors *
-GOTO EXIT
-
 :LINKERROR
-ECHO * Linking errors *
+rem PAUSE * Linking errors *
 GOTO EXIT
-
-:SINTAX
-ECHO    SYNTAX: Build [Program]     {-- No especifiques la extensi¢n PRG
-ECHO                                {-- Don't specify .PRG extension
-GOTO EXIT
-
-:NOEXIST
-ECHO The specified PRG %1 does not exist
 
 :EXIT
-rem delete temporary files
-@del %1.c
-
+pause 
