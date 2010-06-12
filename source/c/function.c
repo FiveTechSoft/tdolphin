5178d1fa002dbc41d42dc612de7fcbd041b27c27
daniel <daniel@94bf062e-bdd7-4c11-962c-b781cd401d63>
2010-06-12 16:45:44 +0000

    > testcon.PRG * forced error connection in testcon
    > tdolpsrv.PRG
    + METHOD CheckError( nError )
    + METHOD Connect( cServer, cUser, cPassword, nPort, nFlags )
       > default values from DATAS
    + METHOD ErrorNo()
    
    > function.c
    + FUNCTION MySqlGetErrNo()
    + FUNCTION MySqlClose()
    * Changed MySqlConnect, now return pointer MYSQL when mysql_init is successful
    
    git-svn-id: http://www.sitasoft.net/tdolphin@2 94bf062e-bdd7-4c11-962c-b781cd401d63

--- source/c/function.c
+++ source/c/function.c
@@ -16,19 +16,25 @@ HB_FUNC( MYSQLCONNECT ) // -> MYSQL*
    mysql = mysql_init( NULL );
    if ( ( mysql != NULL ) )
    {
-     if( mysql_real_connect( mysql, szHost, szUser, szPass, 0, port, NULL, flags ) )
-     {
-        hb_retnl( ( long ) mysql );
-     }
-     else
-     {
-       mysql_close( mysql );
-       hb_retnl( 0 );
-     }
+   	  mysql_real_connect( mysql, szHost, szUser, szPass, 0, port, NULL, flags );
+      hb_retnl( ( long ) mysql );
    }
    else
    {
-
      hb_retnl( 0 );
    }
 }  
+
+//------------------------------------------------//
+//void mysql_close(MYSQL *mysql)
+HB_FUNC( MYSQLCLOSE )//->none
+{
+   mysql_close( ( MYSQL * ) hb_parnl( 1 ) );
+}
+
+//------------------------------------------------//
+//unsigned int mysql_errno(MYSQL *mysql)
+HB_FUNC( MYSQLGETERRNO )//->An error code value for the last mysql_xxx()
+{
+   hb_retnl( mysql_errno( ( MYSQL * ) hb_parnl( 1 ) ) );
+}
