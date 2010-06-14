a510519d84e275d7a884ba8507b9f308d70ad4ef
william <william@94bf062e-bdd7-4c11-962c-b781cd401d63>
2010-06-14 17:20:46 +0000

    Adicionado un script BAT para compilar los ejemplos y este pida el nombre del PRG y no andar modificando el SCRIPT por cada ejemplo
    git-svn-id: http://www.sitasoft.net/tdolphin@20 94bf062e-bdd7-4c11-962c-b781cd401d63

---
+++ samples/wbldtstx.bat
@@ -0,0 +1,79 @@
+@echo off
+color 80
+title pruebas Dolphin 1.0
+set /P wfile= Teclee el nombre del test y presione ENTER ::. 
+
+IF NOT EXIST %wfile%.prg GOTO ERRORFILE
+CLS
+ECHO Compiling...
+set hdir=y:\xharbcvs
+set bcdir=y:\borland\bcc55\bin
+
+%hdir%\bin\harbour %wfile% /n /i..\include;%hdir%\include /p %2 %3 > clip.log
+@type clip.log
+IF ERRORLEVEL 1 PAUSE
+IF ERRORLEVEL 1 GOTO EXIT
+
+echo -O2 -e%wfile%.exe -I%hdir%\include %wfile%.c > b32.bc
+%bcdir%\bcc32 -M -c @b32.bc
+:ENDCOMPILE
+
+echo c0x32.obj + > b32.bc
+echo %wfile%.obj, + >> b32.bc
+echo %wfile%.exe, + >> b32.bc
+echo %wfile%.map, + >> b32.bc
+echo dolphinx.lib + >> b32.bc
+echo ..\lib\libmysql.lib + >> b32.bc
+echo %hdir%\lib\rtl.lib + >> b32.bc
+echo %hdir%\lib\hsx.lib + >> b32.bc
+echo %hdir%\lib\hbsix.lib + >> b32.bc
+echo %hdir%\lib\rtl.lib + >> b32.bc
+echo %hdir%\lib\vm.lib + >> b32.bc
+echo %hdir%\lib\rtl.lib + >> b32.bc
+echo %hdir%\lib\gtwin.lib + >> b32.bc
+echo %hdir%\lib\Lang.lib + >> b32.bc
+echo %hdir%\lib\macro.lib + >> b32.bc
+echo %hdir%\lib\rdd.lib + >> b32.bc
+echo %hdir%\lib\dbfntx.lib + >> b32.bc
+echo %hdir%\lib\dbfcdx.lib + >> b32.bc
+echo %hdir%\lib\common.lib + >> b32.bc
+echo %hdir%\lib\libmisc.lib + >> b32.bc
+echo %hdir%\lib\CodePage.lib + >> b32.bc
+echo %hdir%\lib\DbfFpt.lib + >> b32.bc
+echo %hdir%\lib\pcrepos.lib + >> b32.bc
+
+rem Uncomment these two lines to use Advantage RDD
+rem echo %hdir%\lib\rddads.lib + >> b32.bc 
+rem echo %hdir%\lib\ace32.lib + >> b32.bc
+
+echo %bcdir%\lib\cw32.lib + >> b32.bc
+echo %bcdir%\lib\import32.lib, >> b32.bc
+
+ECHO * 
+ECHO Linking...
+%bcdir%\ilink32 -Gn -Tpe -s @b32.bc
+
+ECHO delete temporary files
+del *.obj
+del *.ppo
+del *.log
+del *.map
+del *.tds
+del *.c
+del *.bc
+
+IF ERRORLEVEL 1 GOTO LINKERROR
+ECHO * Application successfully built
+GOTO EXIT
+
+
+:LINKERROR
+rem PAUSE * Linking errors *
+GOTO EXIT
+
+:ERRORFILE
+ECHO * El archivo %wfile% no existe, verifique... *
+GOTO EXIT
+
+:EXIT
+pause 
