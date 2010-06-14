a21a6b49fc292f6007dff0bbd3bc5d4352c7e3eb
william <william@94bf062e-bdd7-4c11-962c-b781cd401d63>
2010-06-14 23:30:11 +0000

    Clase tdolpsrv.prg  Se adiciona parametros cDBName a:    :New( .. )    :Connect.ini( .. )
    Fuente C function.c
     Se adiciona parametro cDBName a función MYSQLCONNECT(..)
    
    TestDB conexion incluyendo la base de datos para uso
    
    git-svn-id: http://www.sitasoft.net/tdolphin@29 94bf062e-bdd7-4c11-962c-b781cd401d63

--- source/c/function.c
+++ source/c/function.c
@@ -64,10 +64,11 @@ HB_FUNC( MYSQLCONNECT ) // -> MYSQL*
    const char *szPass = ( LPSTR ) hb_parc( 3 );
    unsigned int port  = ISNUM( 4 ) ? ( unsigned int ) hb_parni( 4 ) :  MYSQL_PORT;
    unsigned int flags = ISNUM( 5 ) ? ( unsigned int ) hb_parni( 5 ) :  0;
+   const char *szdb = ISCHAR( 6 ) ? ( LPSTR ) hb_parc( 6 ): 0;
    mysql = mysql_init( NULL );
    if ( ( mysql != NULL ) )
    {
-   	  mysql_real_connect( mysql, szHost, szUser, szPass, 0, port, NULL, flags );
+   	  mysql_real_connect( mysql, szHost, szUser, szPass, szdb, port, NULL, flags );
       hb_retnl( ( long ) mysql );
    }
    else
