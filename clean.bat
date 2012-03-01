cca72b72862c289e2915cdb5e09a5baf61a3fa8f
daniel <daniel@94bf062e-bdd7-4c11-962c-b781cd401d63>
2012-03-01 21:39:47 +0000

    ...
    git-svn-id: http://www.sitasoft.net/tdolphin@255 94bf062e-bdd7-4c11-962c-b781cd401d63

--- clean.bat
+++ clean.bat
@@ -1,13 +1,3 @@
-/* $Id$ */
-/**
-* Nombre del proyecto:  Daniel Garcia-Gil
-*
-* @author         $Author$
-* @version        $Rev$
-* @lastrevision   $Date$
-* @filesource     $URL$
-*/
-
 @call cleanenv
 @win-make clean
 @cd samples
