b7148269d6265642f26c210ff2b01c3efcbd7895
daniel <daniel@94bf062e-bdd7-4c11-962c-b781cd401d63>
2012-07-26 03:10:53 +0000

    added missed file in last commit
    git-svn-id: http://www.sitasoft.net/tdolphin@264 94bf062e-bdd7-4c11-962c-b781cd401d63

--- setenvh.sh
+++ setenvh.sh
@@ -1,9 +1,9 @@
 export ARCHITECTURE=linux
 export PRG_COMPILER=HARBOUR
-export PRG_COMP_PATH=~/harbour
+export PRG_COMP_PATH=/usr/local
 export PRG_COMP_BIN_PATH=$PRG_COMP_PATH/bin
-export PRG_COMP_LIB_PATH=$PRG_COMP_PATH/lib
-export PRG_COMP_INC_PATH=$PRG_COMP_PATH/include
+export PRG_COMP_LIB_PATH=$PRG_COMP_PATH/lib/harbour
+export PRG_COMP_INC_PATH=$PRG_COMP_PATH/include/harbour
 export C_COMPILER=gcc
 export LIBNAME=libdolphin
 export DOLPHIN_INC=./include
